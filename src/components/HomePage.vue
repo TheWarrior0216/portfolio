c2440110ab35583c0a548be6f9cbc3de4d3bad3e
Seth Baxter <TheWarrior0216@gmail.com>
2024-09-04 10:38:42 -0600

    updated template

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -48,7 +48,7 @@ onUnmounted(() => window.removeEventListener('mousemove', update))
         <section class=" text-5xl mb-5 font-bold">
           <div class="">
             <p class="text-slate-200 cursor-pointer">
-              Evan Varozza
+              Seth Baxter
             </p>
           </div>
         </section>
@@ -77,19 +77,19 @@ onUnmounted(() => window.removeEventListener('mousemove', update))
 
       <div class="flex justify-center md:justify-start">
         <div class="card md:mb-0 my-8 ">
-          <a class="social-link1" href="https://github.com/VarozzaEJ">
+          <a class="social-link1" href="https://github.com/TheWarrior0216">
             <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" height="1em" viewBox="0 0 496 512">
               <path
                 d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z">
               </path>
             </svg>
           </a>
-          <a class="social-link2" href="https://www.linkedin.com/in/evan-varozza-521b78293/">
+          <!-- <a class="social-link2" href="https://www.linkedin.com/in/evan-varozza-521b78293/">
             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" fill="#fff">
               <path
                 d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z">
               </path>
-            </svg> </a>
+            </svg> </a> -->
 
         </div>
       </div>
@@ -101,7 +101,7 @@ onUnmounted(() => window.removeEventListener('mousemove', update))
     </div>
     <div class="   text-slate-400 mx-0 lg:mx-0  lg:pt-20 pt-10">
       <p class="lg:hidden text-slate-100 mb-5">About</p>
-      <p id="about" class="md:mb-8 mb-7">Hello! I’m Evan Varozza, a passionate software developer with a knack for
+      <p id="about" class="md:mb-8 mb-7">Hello! I’m Seth Baxter, a passionate software developer with excitment for
         crafting
         innovative
         and efficient
@@ -184,7 +184,7 @@ onUnmounted(() => window.removeEventListener('mousemove', update))
                   src="https://cdn.discordapp.com/attachments/1130957592240332851/1280336417490210897/Screenshot_2024-08-29_at_11.55.46_PM.png?ex=66d9aff5&is=66d85e75&hm=a45aa624f97e92aa8f96d23f1b6ad24b5dae82c6ce0180b082e76c468ace925b&"
                   alt="">
                 <div class="flex flex-col w-full">
-                  <a class="lg:cursor-pointer" href="https://keepr.evanvarozza.dev/#/">
+                  <a class="lg:cursor-pointer" href="https://keepr.sethbaxter.org/#/">
                     <p class="flex mt-2">Keepr <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke-width="1.5" stroke="currentColor"
                         class="size-4 mt-1 ms-5 transition ease-in-out delay-0  hover:-translate-y-1 hover:scale-110">
@@ -192,11 +192,9 @@ onUnmounted(() => window.removeEventListener('mousemove', update))
                           d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                       </svg></p>
                   </a>
-                  <p class="text-slate-400 pe-1">Keepr is a social site where you post "keeps" and save them into
-                    "vaults".
-                    Made
-                    using a c# backend and vue front end, this app incorporates data manipulation using MySQL and
-                    Dapper.
+                  <p class="text-slate-400 pe-1">
+                    Keepr is a website about collections and creating individual posts that you can share to a home page
+                    for everyone to see. You can navigate to profile pages and view other individual vaults.
                   </p>
                   <div class="flex my-3">
                     <span
@@ -223,27 +221,28 @@ onUnmounted(() => window.removeEventListener('mousemove', update))
                 <img class=" h-40 lg:h-32 lg:pe-3 lg:w-72 w-52 pb-3 lg:pb-0"
                   src="https://cdn.discordapp.com/attachments/1130957592240332851/1280643431844020295/Screenshot_2024-09-03_at_3.39.04_PM.png?ex=66d8d3a3&is=66d78223&hm=9555b3fa7723ca7d9941fcb411b2210cc557a5f6236c663e8e3ebacf5640d682&"
                   alt="">
-                <div class="flex flex-col w-full">
-                  <a class="lg:cursor-pointer" href="https://tower.evanvarozza.dev/#/">
-                    <p class="flex mt-2">Tower<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                <div class="flex flex-col w-5/6">
+                  <a class="lg:cursor-pointer" href="https://boisemenuworks.com/#/">
+                    >>>>>>> 7b535a4 (Updated Template)
+                    <p class="flex mt-2">Jot<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke-width="1.5" stroke="currentColor"
                         class="size-4 mt-1 ms-5 transition ease-in-out delay-0  hover:-translate-y-1 hover:scale-110">
                         <path stroke-linecap="round" stroke-linejoin="round"
                           d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                       </svg></p>
                   </a>
-                  <p class="text-slate-400 pe-2">Tower simplifies event planning with an intuitive platform for
-                    creating, managing, and promoting events. Enjoy features like customizable pages, easy RSVP
-                    tracking, and real-time analytics to ensure your event’s success. Elevate your event experience with
-                    Tower.
+                  <p class="text-slate-400 pe-2">
+                    Menu Works the application created for the small buisness owner and anybody who eats food. Find a
+                    local buisness that you may have never known about and order directly from your device. The small
+                    buisness owner can receive orders and add there menu with ease.
                   </p>
                   <div class="flex my-3">
                     <span
-                      class="bg-green-100  text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-11">Vue</span>
+                      class="bg-green-100  text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-22">Javascript</span>
                     <span
-                      class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-18">NodeJS</span>
+                      class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-12">CSS</span>
                     <span
-                      class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-15">MongoDB</span>
+                      class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-15">HTML</span>
                   </div>
                 </div>
               </div>
@@ -295,37 +294,6 @@ onUnmounted(() => window.removeEventListener('mousemove', update))
               y: 0,
             }">
 
-              <div
-                class="flex mb-5 hover:bg-slate-50/10 hover:text-purple-400 transition-all ease-in-out delay-0 hover:-translate-y-1 hover:backdrop-blur-sm  hover:blur-none   gap-4 flex-col-reverse ps-3 pt-2 lg:p-0 lg:ps-2 items-start lg:items-center lg:hover:!opacity-100 lg:group-hover/list:opacity-50 justify-start md:justify-center rounded-lg bg-slate-900 text-surface shadow-secondary-1 dark:bg-surface-dark lg:-inset-x-6 lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg dark:text-white lg:flex-row">
-
-                <img class="h-40 lg:h-32 lg:pe-3 lg:w-72 w-52 pb-3 lg:pb-0"
-                  src="https://cdn.discordapp.com/attachments/1130957592240332851/1280376292600184882/Screenshot_2024-09-02_at_9.58.04_PM.png?ex=66d92c58&is=66d7dad8&hm=f65f61d5941e75c137ad71b443b1856ce99db60d6d0352a1b9cfe30abe1d196e&"
-                  alt="">
-                <div class="flex flex-col w-full">
-                  <a class="lg:cursor-pointer" href="https://boisepetworks.org/#/">
-
-                    <p class="flex mt-2">Petworks<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke-width="1.5" stroke="currentColor"
-                        class="size-4 mt-1 ms-5 transition ease-in-out delay-0  hover:-translate-y-1 hover:scale-110">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                      </svg></p>
-                  </a>
-                  <p class="text-slate-400 pe-2">Petworks is the perfect place for pet lovers to create profiles for
-                    their
-                    pets, share updates and photos, and connect with other pet enthusiasts. Celebrate your pets and join
-                    a community that’s as passionate about animals as you are!
-                  </p>
-                  <div class="flex my-3">
-                    <span
-                      class="bg-green-100  text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-11">Vue</span>
-                    <span
-                      class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-18">NodeJS</span>
-                    <span
-                      class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-15">MongoDB</span>
-                  </div>
-                </div>
-              </div>
             </li>
             <li v-motion :initial="{
               opacity: 0,
@@ -334,38 +302,6 @@ onUnmounted(() => window.removeEventListener('mousemove', update))
               opacity: 1,
               y: 0,
             }">
-
-              <div
-                class="flex mb-5 hover:bg-slate-50/10 hover:text-purple-400 transition-all ease-in-out delay-0 hover:-translate-y-1 hover:backdrop-blur-sm  hover:blur-none  gap-4  flex-col-reverse ps-3 pt-2 lg:p-0 lg:ps-2 items-start lg:items-center lg:hover:!opacity-100 lg:group-hover/list:opacity-50 justify-start md:justify-center rounded-lg bg-slate-900 text-surface shadow-secondary-1 dark:bg-surface-dark lg:-inset-x-6 lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg dark:text-white lg:flex-row">
-
-                <img class="h-40 lg:h-32 lg:pe-3 lg:w-72 w-52 pb-3 lg:pb-0"
-                  src="https://cdn.discordapp.com/attachments/1130957592240332851/1280375899010629704/Screenshot_2024-09-02_at_9.56.11_PM.png?ex=66d92bfa&is=66d7da7a&hm=e01af7bceab236ebd855b7a03d76c508c45ab7b5080a166f24abdef1cfe6ef11&"
-                  alt="">
-                <div class="flex flex-col w-full">
-                  <a class="lg:cursor-pointer" href="https://varozzaej.github.io/">
-
-                    <p class="flex mt-2">Balloon Pop<svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                        class="size-4 mt-1 ms-5 transition ease-in-out delay-0  hover:-translate-y-1 hover:scale-110">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                      </svg></p>
-                  </a>
-                  <p class="text-slate-400 pe-2">Balloon Pop is a fast-paced, addictive web app where the goal is
-                    simple: pop
-                    as many balloons as you can before time runs out! Test your reflexes and see how quickly you can
-                    burst colorful balloons in this fun and challenging game.
-                  </p>
-                  <div class="flex my-3">
-                    <span
-                      class="bg-green-100  text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-22">Javascript</span>
-                    <span
-                      class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-12">CSS</span>
-                    <span
-                      class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 w-15">HTML</span>
-                  </div>
-                </div>
-              </div>
             </li>
 
           </ol>
